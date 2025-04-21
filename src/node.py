36b43b00644226ed919ed7c3a2494831a9317521
Anastasiia Rynkevych <nestalirunkevich@gmail.com>
2025-04-21 21:32:02 +0300

    feat: define basic BPlusTreeNode structure with leaf linkage

---
+++ src/node.py
@@ -0,0 +1,7 @@
+class BPlusTreeNode:
+    def __init__(self, is_leaf=False):
+        self.is_leaf = is_leaf
+        self.keys = []
+        self.children = []
+        self.parent = None
+        self.next_leaf = None
